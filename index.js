c19151f8fdec46c67d1e42b36cdc6c3d9a55f0cc
Stephane Demotte <stephane@demotte.net>
2017-04-26 15:16:02 -0400

    Update index.js

--- index.js
+++ index.js
@@ -49,7 +49,8 @@ const defaultProps = {
     cancelStyle: {},
     cancelTextStyle: {},
     overlayStyle: {},
-    cancelText: 'cancel'
+    cancelText: 'cancel',
+    containerStyle: {}
 };
 
 export default class ModalPicker extends BaseComponent {
@@ -130,7 +131,7 @@ export default class ModalPicker extends BaseComponent {
 
         return (
             <View style={[styles.overlayStyle, this.props.overlayStyle]} key={'modalPicker'+(componentIndex++)}>
-                <View style={styles.optionContainer}>
+                <View style={[styles.optionContainer, this.props.containerStyle]}>
                     <ScrollView keyboardShouldPersistTaps="always">
                         <View style={{paddingHorizontal:10}}>
                             {options}
